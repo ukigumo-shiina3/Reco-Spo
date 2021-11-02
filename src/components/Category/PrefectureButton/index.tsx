f44ad40ec715efc2d517921a382c161c0f61d2e0
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-11-02 22:19:01 +0900

    ✨  カテゴリーボタン追加

---
+++ src/components/Category/PrefectureButton/index.tsx
@@ -0,0 +1,5 @@
+export const PrefectureButton: React.VFC = () => {
+  return (
+    <button className=' bg-red-400 text-white text-xs font-bold py-2 px-5 rounded '>石川県</button>
+  );
+};
---
+++ src/components/Category/SystemButton/index.tsx
@@ -0,0 +1,7 @@
+export const SystemButton: React.VFC = () => {
+  return (
+    <button className=' bg-yellow-400  text-white text-xs font-bold p-2 rounded '>
+      お試し暮らし
+    </button>
+  );
+};
