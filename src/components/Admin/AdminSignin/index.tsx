54c0b72af1fefd857fdf4ce4ea27a53101ed16c0
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-11-17 11:39:46 +0900

    💄  フォームとボタン幅の調整

--- src/components/Admin/AdminSignin/index.tsx
+++ src/components/Admin/AdminSignin/index.tsx
@@ -1,14 +1,9 @@
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import toast from 'react-hot-toast';
-import { ReactNode, useState } from 'react';
+import { useState } from 'react';
 import { supabase } from 'src/libs/supabase';
-
-type Props = {
-  admin_email: string;
-  admin_password: string;
-  children: ReactNode;
-};
 
 export const AdminSignin: React.VFC = () => {
   const [email, setEmail] = useState('');
@@ -37,53 +32,52 @@ export const AdminSignin: React.VFC = () => {
           />
         </div>
         <div className='z-10 w-full rounded overflow-hidden shadow-lg  mr-0 ml-auto md:w-6/12 bg-white'>
-          <div className='p-10 ml-10'>
+          <div className='p-10 sm:mx-10'>
             <div className='font-bold text-2xl text-center mb-2'>ログイン</div>
-            <label htmlFor='name' className='flex justify-start pt-10 pb-3'>
+            <label htmlFor='email' className='flex justify-start pt-10 pb-3'>
               メールアドレス
             </label>
             <input
               type='text'
               name='email'
               value={email}
-              id='admin_email'
+              id='email'
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
               placeholder='reco-spo@gmail.com'
-              className='w-2/3 p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
+              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
             />
-            <label htmlFor='name' className='flex justify-start pt-10 pb-3'>
+            <label htmlFor='password' className='flex justify-start pt-10 pb-3'>
               パスワード
             </label>
             <input
               type='text'
               name='password'
               value={password}
-              id='admin_password'
+              id='password'
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
               placeholder='test1234'
-              className='w-2/3 p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
+              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
             />
 
-            <div className='flex pl-4 pb-10'>
+            <div className='flex pr-4 pb-10'>
               <Link href='/admins' passHref>
                 <button
                   onClick={HandleSignin}
-                  className='px-4 py-2 mt-10 mx-6 text-white bg-blue-300 rounded-lg'
+                  className='px-6 py-3 mt-10 mr-4 text-white bg-blue-300 rounded-lg'
                 >
                   ログイン
                 </button>
               </Link>
-              <div className='mt-10'>
+              <div className='mt-10 md:ml-5'>
                 {/* <p className='text-sm border-b-2 '>ログイン情報をお忘れですか？</p> */}
                 <Link href='/admins/signup' passHref>
                   <a className='text-sm border-b-2 pt-2'>新規会員登録はこちら</a>
                 </Link>
               </div>
-              ;
             </div>
           </div>
         </div>
--- src/components/Admin/AdminSignup/index.tsx
+++ src/components/Admin/AdminSignup/index.tsx
@@ -1,14 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import toast from 'react-hot-toast';
-import { ReactNode, useState } from 'react';
+import { useState } from 'react';
 import { supabase } from 'src/libs/supabase';
-
-type Props = {
-  admin_email: string;
-  admin_password: string;
-  children: ReactNode;
-};
 
 export const AdminSignup: React.VFC = () => {
   const [email, setEmail] = useState('');
@@ -26,8 +20,8 @@ export const AdminSignup: React.VFC = () => {
 
   return (
     <div>
-      <div className='flex'>
-        <div className='hidden md:block z-0 w-6/12 '>
+      <div className='flex h-full'>
+        <div className='hidden md:block z-0 w-1/2 '>
           <Image
             src='/auth-pic.jpg'
             layout='fill'
@@ -37,43 +31,43 @@ export const AdminSignup: React.VFC = () => {
           />
         </div>
 
-        <div className='z-10 w-full rounded overflow-hidden shadow-lg  mr-0 ml-auto md:w-6/12 bg-white'>
-          <div className='p-10 ml-10'>
+        <div className='z-10 w-full rounded overflow-hidden shadow-lg  mr-0 ml-auto md:w-1/2 bg-white'>
+          <div className='p-10 mx-10'>
             <div className='font-bold text-2xl text-center mb-2'>新規登録</div>
-            <label htmlFor='name' className='flex justify-start pt-10 pb-3'>
+            <label htmlFor='email' className='flex justify-start pt-10 pb-3'>
               メールアドレス
             </label>
             <input
               type='text'
               name='email'
               value={email}
-              id='admin_email'
+              id='email'
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
               placeholder='reco-spo@gmail.com'
-              className='w-2/3 p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
+              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
             />
-            <label htmlFor='name' className='flex justify-start pt-10 pb-3'>
+            <label htmlFor='password' className='flex justify-start pt-10 pb-3'>
               パスワード
             </label>
             <input
               type='text'
               name='password'
               value={password}
-              id='admin_password'
+              id='password'
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
               placeholder='test1234'
-              className='w-2/3 p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
+              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
             />
 
             <div className='flex justify-center  pl-4 pb-10'>
               <Link href='/admins' passHref>
                 <button
                   onClick={HandleSignup}
-                  className='px-4 py-2 mt-10 mx-6 text-white bg-blue-300 rounded-lg'
+                  className='px-6 py-3 mt-10 mx-6 text-white bg-blue-300 rounded-lg'
                 >
                   新規登録
                 </button>
