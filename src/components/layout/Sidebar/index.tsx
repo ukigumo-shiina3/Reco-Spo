181d26d605810a18a7a1ecce961ae56dec5532cb
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-11-20 17:58:19 +0900

    ✨  ログアウト機能追加

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { supabase } from 'src/libs/supabase';
+
+const HandleLogout = () => {
+  supabase.auth.signOut();
+};
 
 export const Sidebar: React.VFC = () => {
   return (
     <div>
-      <div className='w-28 h-screen text-center bg-blue-300 md:w-56 '>
+      <div className='flex-1 w-28 h-full text-center bg-blue-300 md:w-56 '>
         <div className='bg-blue-400 py-10'>
           <a className='font-fancy font-bold text-white text-xl md:text-4xl'>Reco Spo</a>
         </div>
@@ -20,15 +26,26 @@ export const Sidebar: React.VFC = () => {
             <p className='text-xs pb-2 md:text-sm'>穴水役場さん</p>
           </div>
         </div>
-        <p className='text-xs text-center text-white  hover:bg-blue-400 py-8 lg:text-sm '>
-          プロフィール編集
-        </p>
-        <p className='text-xs text-center text-white  hover:bg-blue-400 py-8 lg:text-sm '>
-          スポット投稿
-        </p>
-        <p className='text-xs text-center text-white  hover:bg-blue-400 py-8 lg:text-sm '>
-          スポット編集
-        </p>
+        <div className='flex flex-col'>
+          <Link href='/admins/' passHref>
+            <a className='text-xs text-center text-white  hover:bg-blue-400 py-8 lg:text-sm '>
+              プロフィール編集
+            </a>
+          </Link>
+          <Link href='/admins/spots/post' passHref>
+            <a className='text-xs text-center text-white  hover:bg-blue-400 py-8 lg:text-sm '>
+              スポット投稿
+            </a>
+          </Link>
+          {/* <Link href={`/${spots.id}`} passHref> */}
+          <a className='text-xs text-center text-white  hover:bg-blue-400 py-8 lg:text-sm '>
+            スポット編集
+          </a>
+          {/* </Link> */}
+          <a className='text-xs text-center text-white  hover:bg-blue-400 py-8 lg:text-sm '>
+            <button onClick={HandleLogout}>ログアウト</button>
+          </a>
+        </div>
       </div>
     </div>
   );
