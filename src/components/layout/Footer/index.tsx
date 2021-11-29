e410f177eb7d9143be25bda52117dc93edb43c37
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-11-29 22:58:42 +0900

    ✏️ リンク先URLの変更

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
+import type { VFC } from 'react';
 
-export const Footer: React.VFC = () => {
+export const Footer: VFC = () => {
   return (
     <div className='text-center block justify-between  bg-gray-200 md:flex  '>
       <div className='text-xs p-4 md:text-sm md:p-7'>
         Copyright © 2021 Reco Spo. All rights reserved{' '}
       </div>
       <div className='md:p-6'>
-        <Link href='/terms' passHref>
+        <Link href='/term' passHref>
           <a className='text-xs pr-6 md:text-sm md:p-6'>利用規約</a>
         </Link>
         <Link href='/policy' passHref>
