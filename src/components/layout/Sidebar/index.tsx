5cceeec144ed7aa264bdd3433ebea82aa0a0b13c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-01 00:07:25 +0900

    ⚡️ 再レンダリング最適化

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import type { VFC } from 'react';
+import { useCallback } from 'react';
 import { supabase } from 'src/libs/supabase';
 
-const HandleLogout = () => {
-  supabase.auth.signOut();
-};
+export const Sidebar: VFC = () => {
+  const HandleLogout = useCallback(() => {
+    supabase.auth.signOut();
+  }, []);
 
-export const Sidebar: React.VFC = () => {
   return (
     <div>
       <div className='flex-1 w-28 h-full text-center bg-blue-300 md:w-56 '>
         <div className='bg-blue-400 py-10'>
           <a className='font-fancy font-bold text-white text-xl md:text-4xl'>Reco Spo</a>
         </div>
-        <div className='py-8 px-1 sm:flex justify-center '>
+        <div className='py-8 px-1 md:flex justify-center '>
           <Image
             src='/spot-pic.jpeg'
             alt='admin_image'
@@ -23,7 +25,7 @@ export const Sidebar: React.VFC = () => {
           />
           <div className='text-white p-3'>
             <p className='text-xs pb-2 md:text-sm'>ようこそ</p>
-            <p className='text-xs pb-2 md:text-sm'>穴水役場さん</p>
+            <p className='text-xs pb-2 md:text-sm'>自治体担当者様</p>
           </div>
         </div>
         <div className='flex flex-col'>
