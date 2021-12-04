943be2eaa1c1a3753059e19493fd424ec045af6c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-05 00:09:03 +0900

    ♻️ 認証ページの大枠レイアウト共通化

--- src/components/Admin/AdminSignin/index.tsx
+++ src/components/Admin/AdminSignin/index.tsx
@@ -7,6 +7,7 @@ import { supabase } from 'src/libs/supabase';
 import { useRouter } from 'next/router';
 import { Session } from '@supabase/supabase-js';
 import { toast, Toaster } from 'react-hot-toast';
+import { AdminAuthLayout } from 'src/components/layout/AdminAuthLayout';
 
 type AdminSigninProps = {
   title: string;
@@ -55,71 +56,69 @@ export const AdminSignin: VFC<AdminSigninProps> = (props, session) => {
   }, [email]);
 
   return (
-    <div>
-      <div className='flex h-screen'>
-        <div className='hidden md:block z-0 w-1/2 '>
-          <Image
-            src='/auth-pic.jpg'
-            layout='fill'
-            objectFit='cover'
-            quality={100}
-            alt='管理画面画像'
+    <AdminAuthLayout>
+      <div className='hidden md:block z-0 w-1/2 '>
+        <Image
+          src='/auth-pic.jpg'
+          layout='fill'
+          objectFit='cover'
+          quality={100}
+          alt='管理画面画像'
+        />
+      </div>
+      <div className='z-10 h-screen w-full rounded overflow-hidden shadow-2xl mr-0 ml-auto my-auto md:w-1/2 bg-gray-200'>
+        <div className='p-10 md:p-15 my-20  bg-white xs:mx-16 sm:mx-20'>
+          <div className='font-bold text-2xl text-center mb-2'>{props.title}</div>
+          <label htmlFor='email' className='flex justify-start pt-10 pb-3'>
+            メールアドレス
+          </label>
+          <input
+            type='text'
+            name='email'
+            value={email}
+            id='email'
+            onChange={(e) => {
+              setEmail(e.target.value.trim());
+            }}
+            placeholder='reco-spo@gmail.com'
+            className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
           />
-        </div>
-        <div className='z-10 h-screen w-full rounded overflow-hidden shadow-2xl mr-0 ml-auto my-auto md:w-1/2 bg-gray-200'>
-          <div className='p-10 md:p-15 my-20  bg-white xs:mx-16 sm:mx-20'>
-            <div className='font-bold text-2xl text-center mb-2'>{props.title}</div>
-            <label htmlFor='email' className='flex justify-start pt-10 pb-3'>
-              メールアドレス
-            </label>
-            <input
-              type='text'
-              name='email'
-              value={email}
-              id='email'
-              onChange={(e) => {
-                setEmail(e.target.value.trim());
-              }}
-              placeholder='reco-spo@gmail.com'
-              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
-            />
-            <label htmlFor='password' className='flex justify-start pt-10 pb-3'>
-              パスワード
-            </label>
-            <input
-              type='text'
-              name='password'
-              value={password}
-              id='password'
-              onChange={(e) => {
-                setPassword(e.target.value.trim());
-              }}
-              placeholder='test1234'
-              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
-            />
+          <label htmlFor='password' className='flex justify-start pt-10 pb-3'>
+            パスワード
+          </label>
+          <input
+            type='text'
+            name='password'
+            value={password}
+            id='password'
+            onChange={(e) => {
+              setPassword(e.target.value.trim());
+            }}
+            placeholder='test1234'
+            className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
+          />
 
-            <div className='flex pr-4 pb-10'>
-              <Link href='/admins' passHref>
-                <button
-                  onClick={HandleSignin}
-                  className='px-5 py-1 mt-10 mr-4 text-white bg-blue-300 rounded-lg'
-                >
-                  {props.button}
-                </button>
+          <div className='flex pr-4 pb-10'>
+            <Link href='/admins' passHref>
+              <button
+                onClick={HandleSignin}
+                className='px-5 py-1 mt-10 mr-4 text-white bg-blue-300 rounded-lg'
+              >
+                {props.button}
+              </button>
+            </Link>
+            <div className='flex flex-col mt-12 md:ml-5'>
+              <a className='text-sm border-b-2 '>
+                <button onClick={HandleResetPassword}> ログイン情報をお忘れですか？</button>
+              </a>
+              <Link href='/admins/signup' passHref>
+                <a className='text-sm border-b-2 pt-2'>新規会員登録はこちら</a>
               </Link>
-              <div className='flex flex-col mt-12 md:ml-5'>
-                <a className='text-sm border-b-2 '>
-                  <button onClick={HandleResetPassword}> ログイン情報をお忘れですか？</button>
-                </a>
-                <Link href='/admins/signup' passHref>
-                  <a className='text-sm border-b-2 pt-2'>新規会員登録はこちら</a>
-                </Link>
-              </div>
-              <Toaster />
             </div>
+            <Toaster />
           </div>
         </div>
       </div>
-    </div>
+    </AdminAuthLayout>
   );
 };
--- src/components/Admin/AdminSignup/index.tsx
+++ src/components/Admin/AdminSignup/index.tsx
@@ -6,6 +6,7 @@ import { supabase } from 'src/libs/supabase';
 import { Session } from '@supabase/supabase-js';
 import { toast, Toaster } from 'react-hot-toast';
 import { useRouter } from 'next/router';
+import { AdminAuthLayout } from 'src/components/layout/AdminAuthLayout';
 
 type AdminSignupProps = {
   title: string;
@@ -46,64 +47,62 @@ export const AdminSignup: VFC<AdminSignupProps> = (props, session) => {
   }, [session]);
 
   return (
-    <div>
-      <div className='flex h-screen'>
-        <div className='hidden md:block z-0 w-1/2 '>
-          <Image
-            src='/auth-pic.jpg'
-            layout='fill'
-            objectFit='cover'
-            quality={100}
-            alt='管理画面画像'
+    <AdminAuthLayout>
+      <div className='hidden md:block z-0 w-1/2 '>
+        <Image
+          src='/auth-pic.jpg'
+          layout='fill'
+          objectFit='cover'
+          quality={100}
+          alt='管理画面画像'
+        />
+      </div>
+
+      <div className='z-10 w-full rounded overflow-hidden shadow-2xl mr-0 ml-auto md:w-1/2 bg-gray-200'>
+        <div className='p-10 my-20 bg-white xs:mx-16 md:mx-20'>
+          <div className='font-bold text-2xl text-center mb-2'>{props.title}</div>
+          <label htmlFor='email' className='flex justify-start pt-10 pb-3'>
+            メールアドレス
+          </label>
+          <input
+            type='text'
+            name='email'
+            value={email}
+            id='email'
+            onChange={(e) => {
+              setEmail(e.target.value.trim());
+            }}
+            placeholder='reco-spo@gmail.com'
+            className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
           />
-        </div>
+          <label htmlFor='password' className='flex justify-start pt-10 pb-3'>
+            パスワード
+          </label>
+          <input
+            type='text'
+            name='password'
+            value={password}
+            id='password'
+            onChange={(e) => {
+              setPassword(e.target.value.trim());
+            }}
+            placeholder='test1234'
+            className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
+          />
 
-        <div className='z-10 w-full rounded overflow-hidden shadow-2xl mr-0 ml-auto md:w-1/2 bg-gray-200'>
-          <div className='p-10 my-20 bg-white xs:mx-16 md:mx-20'>
-            <div className='font-bold text-2xl text-center mb-2'>{props.title}</div>
-            <label htmlFor='email' className='flex justify-start pt-10 pb-3'>
-              メールアドレス
-            </label>
-            <input
-              type='text'
-              name='email'
-              value={email}
-              id='email'
-              onChange={(e) => {
-                setEmail(e.target.value.trim());
-              }}
-              placeholder='reco-spo@gmail.com'
-              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
-            />
-            <label htmlFor='password' className='flex justify-start pt-10 pb-3'>
-              パスワード
-            </label>
-            <input
-              type='text'
-              name='password'
-              value={password}
-              id='password'
-              onChange={(e) => {
-                setPassword(e.target.value.trim());
-              }}
-              placeholder='test1234'
-              className='w-full p-2 bg-gray-200 rounded-l-md placeholder-gray-500'
-            />
-
-            <div className='flex justify-center  pl-4 pb-10'>
-              <Link href='/admins' passHref>
-                <button
-                  onClick={HandleSignup}
-                  className='px-6 py-3 mt-10 mx-6 text-white bg-blue-300 rounded-lg'
-                >
-                  {props.button}
-                </button>
-              </Link>
-            </div>
-            <Toaster />
+          <div className='flex justify-center  pl-4 pb-10'>
+            <Link href='/admins' passHref>
+              <button
+                onClick={HandleSignup}
+                className='px-6 py-3 mt-10 mx-6 text-white bg-blue-300 rounded-lg'
+              >
+                {props.button}
+              </button>
+            </Link>
           </div>
+          <Toaster />
         </div>
       </div>
-    </div>
+    </AdminAuthLayout>
   );
 };
