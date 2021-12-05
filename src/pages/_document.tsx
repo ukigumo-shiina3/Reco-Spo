e83c78d2e72f6d99188116a2fa01f5780c604978
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-06 00:20:25 +0900

    ✨ ファビコンの設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return initialProps;
@@ -8,12 +7,20 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang='ja'>
         <Head>
           <link
             href='https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap'
             rel='stylesheet'
           ></link>
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff' />
         </Head>
         <body>
           <Main />
@@ -23,5 +30,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
