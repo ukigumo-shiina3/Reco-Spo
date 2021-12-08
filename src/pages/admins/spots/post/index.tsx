44f8a2f8a54a4f801dee9e5c1669e8bdc3b84c95
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-08 22:04:54 +0900

    💄  カメラアイコン画像のサイズ修正

--- src/pages/admins/spots/post/index.tsx
+++ src/pages/admins/spots/post/index.tsx
@@ -120,21 +120,21 @@ const SpotsPost: VFC<Props> = (props) => {
             <h2 className='flex mt-5'>
               スポット画像<p className=''>(最大5枚)</p>
             </h2>
-            <div className='flex flex-wrap gap-3 mt-5'>
-              <div className='bg-white w-12 h-12'>
-                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-2 w-8 h-8' />
+            <div className='flex flex-wrap gap-2 mt-5'>
+              <div className='bg-white w-16 h-16'>
+                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-4 w-8 h-8' />
               </div>
-              <div className='bg-white w-12 h-12'>
-                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-2 w-8 h-8' />
+              <div className='bg-white w-16 h-16'>
+                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-4 w-8 h-8' />
               </div>
-              <div className='bg-white w-12 h-12'>
-                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-2 w-8 h-8' />
+              <div className='bg-white w-16 h-16'>
+                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-4 w-8 h-8' />
               </div>
-              <div className='bg-white w-12 h-12'>
-                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-2 w-8 h-8' />
+              <div className='bg-white w-16 h-16'>
+                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-4 w-8 h-8' />
               </div>
-              <div className='bg-white w-12 h-12'>
-                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-2 w-8 h-8' />
+              <div className='bg-white w-16 h-16'>
+                <img src='/camera-icon.png' alt='カメラアイコン' className='m-auto mt-4 w-8 h-8' />
               </div>
             </div>
             <div className='mt-5'>
