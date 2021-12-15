4bc148a1aa05d55b684e669aca4f6a4537b197f1
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-15 14:21:58 +0900

    💬  説明文の修正

--- src/components/System/SysyemIntroduction/index.tsx
+++ src/components/System/SysyemIntroduction/index.tsx
@@ -11,7 +11,7 @@ export const SysyemIntroduction: VFC = () => {
               <img
                 src='/about-pic1.png'
                 alt='制度概要の画像'
-                className='m-auto w-full h-80 sm:h-96 sm:object-fill xl:h-full'
+                className='m-auto w-full h-80 sm:h-full sm:object-fill'
               />
               <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
                 <p>仕事に関する助成金とは？ </p>
@@ -37,9 +37,11 @@ export const SysyemIntroduction: VFC = () => {
                 className='m-auto w-full h-80 sm:h-96 xl:h-full'
               />
               <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
-                <p>仕事に関する助成金とは？ </p>
-                <p className='pt-10'>起業・継業、就職、就農などに対しての補助金制度です。</p>
-                <p className='pt-3 pb-10'>漁師や船員など特定の仕事に対しての補助金もあります。</p>
+                <p>住宅建築補助とは？ </p>
+                <p className='pt-10'>住宅を取得される方」を対象とした支援事業を実施。</p>
+                <p className='pt-3 pb-10'>
+                  申請に基づき住宅取得費の一部を「支援金」として補助する制度。
+                </p>
                 <div className='flex justify-center'>
                   <img
                     src='/property-button.png'
@@ -62,9 +64,11 @@ export const SysyemIntroduction: VFC = () => {
                 className='m-auto w-full h-80 sm:h-96 xl:h-full'
               />
               <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
-                <p>仕事に関する助成金とは？ </p>
-                <p className='pt-10'>起業・継業、就職、就農などに対しての補助金制度です。</p>
-                <p className='pt-3 pb-10'>漁師や船員など特定の仕事に対しての補助金もあります。</p>
+                <p>定住促進奨励金とは？ </p>
+                <p className='pt-10'>定住の促進を目的として交付される助成金であり、</p>
+                <p className='pt-3 pb-10'>
+                  居住の用に供する住宅を購入することを条件に交付される奨励金
+                </p>
                 <div className='flex justify-center'>
                   <img
                     src='/property-button.png'
@@ -85,9 +89,13 @@ export const SysyemIntroduction: VFC = () => {
                 className='m-auto w-full h-80 sm:h-96 xl:h-full'
               />
               <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
-                <p>仕事に関する助成金とは？ </p>
-                <p className='pt-10'>起業・継業、就職、就農などに対しての補助金制度です。</p>
-                <p className='pt-3 pb-10'>漁師や船員など特定の仕事に対しての補助金もあります。</p>
+                <p>お試し移住体験とは </p>
+                <p className='pt-10'>
+                  田舎暮らしへの興味、移住を検討しているといった人を対象に一定の
+                </p>
+                <p className='pt-3 pb-10'>
+                  期間実生活ができるように町が所有している施設を借し出す制度
+                </p>
                 <div className='flex justify-center'>
                   <img
                     src='/property-button.png'
