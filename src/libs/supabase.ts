db14f3e19040ea3da846c69db3b723902e1f2226
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-18 22:19:50 +0900

    🏷️  型アサーション追加

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
 
 if (!SUPABASE_URL) {
   throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
@@ -10,4 +10,4 @@ if (!SUPABASE_KEY) {
   throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_KEY');
 }
 
-export const client = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
