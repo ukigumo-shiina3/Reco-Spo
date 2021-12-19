26ea5c86464c4d7c5ff76ec43cbe8a773c80a05c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-19 16:51:16 +0900

    ✨  スポットカードデータ取得の処理切り出し

---
+++ src/hooks/useSpotCardSelect.tsx
@@ -0,0 +1,9 @@
+import { supabase } from 'src/libs/supabase';
+
+export const getSpots = async () => {
+  const { data, error } = await supabase.from('spots').select('*');
+  if (error) {
+    alert(error);
+  }
+  return data;
+};
