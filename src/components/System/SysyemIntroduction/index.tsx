45a5f08c734904e8b3cb07244795f0fbe090daef
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-19 13:47:54 +0900

    📱  レスポンシブ対応

--- src/components/System/SysyemIntroduction/index.tsx
+++ src/components/System/SysyemIntroduction/index.tsx
@@ -13,9 +13,9 @@ export const SysyemIntroduction: VFC = () => {
               <img
                 src='/about-pic1.png'
                 alt='制度概要の画像'
-                className='m-auto w-full h-80 sm:h-96 xl:h-full xl:object-cover'
+                className='m-auto w-full h-80 sm:h-96'
               />
-              <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
+              <div className='absolute text-white text-center top-16 py-5 px-8 sm:top-20 sm:left-10 2xl:top-50 2xl:left-32'>
                 <p>仕事に関する助成金とは？ </p>
                 <p className='pt-10'>起業・継業、就職、就農などに対しての補助金制度です。</p>
                 <p className='pt-3 pb-10'>漁師や船員など特定の仕事に対しての補助金もあります。</p>
@@ -41,14 +41,12 @@ export const SysyemIntroduction: VFC = () => {
               <img
                 src='/about-pic2.png'
                 alt='制度概要の画像'
-                className='m-auto w-full h-80 sm:h-96 xl:h-full'
+                className='m-auto w-full h-80 sm:h-96'
               />
-              <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
+              <div className='absolute text-white text-center top-16 py-5 px-10 sm:px-14 sm:top-20 sm:left-10 2xl:top-50 2xl:left-32'>
                 <p>住宅建築補助とは？ </p>
                 <p className='pt-10'>住宅を取得される方」を対象とした支援事業を実施。</p>
-                <p className='pt-3 pb-10'>
-                  申請に基づき住宅取得費の一部を「支援金」として補助する制度。
-                </p>
+                <p className='pt-3 pb-10'>住宅取得費の一部を「支援金」として補助する制度。</p>
                 <div className='flex justify-center'>
                   <Link
                     href='https://www.iju-join.jp/material/files/group/1/45c28765f28bde68.pdf'
@@ -73,14 +71,12 @@ export const SysyemIntroduction: VFC = () => {
               <img
                 src='/about-pic3.png'
                 alt='制度概要の画像'
-                className='m-auto w-full h-80 sm:h-96 xl:h-full'
+                className='m-auto w-full h-80 sm:h-96'
               />
-              <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
+              <div className='absolute text-white text-center top-16 py-5 px-12 sm:px-14 sm:top-20 sm:left-10 2xl:top-50 2xl:left-32'>
                 <p>定住促進奨励金とは？ </p>
                 <p className='pt-10'>定住の促進を目的として交付される助成金であり、</p>
-                <p className='pt-3 pb-10'>
-                  居住の用に供する住宅を購入することを条件に交付される奨励金
-                </p>
+                <p className='pt-3 pb-10'>住宅を購入することを条件に交付される奨励金</p>
                 <div className='flex justify-center'>
                   <Link
                     href='https://www.city.satsumasendai.lg.jp/www/contents/1270046749584/index.html'
@@ -103,16 +99,12 @@ export const SysyemIntroduction: VFC = () => {
               <img
                 src='/about-pic4.png'
                 alt='制度概要の画像'
-                className='m-auto w-full h-80 sm:h-96 xl:h-full'
+                className='m-auto w-full h-80 sm:h-96'
               />
-              <div className='absolute text-white text-center top-16 p-5 sm:top-20 sm:left-10 2xl:top-40 2xl:left-32'>
+              <div className='absolute text-white text-center top-16 py-5 px-12 sm:px-14 sm:top-20 sm:left-10 2xl:top-50 2xl:left-32'>
                 <p>お試し移住体験とは </p>
-                <p className='pt-10'>
-                  田舎暮らしへの興味、移住を検討しているといった人を対象に一定の
-                </p>
-                <p className='pt-3 pb-10'>
-                  期間実生活ができるように町が所有している施設を借し出す制度
-                </p>
+                <p className='pt-10'>田舎暮らしの移住を検討している人を対象に一定の</p>
+                <p className='pt-3 pb-10'>期間実生活ができるように町が施設を借し出す制度</p>
                 <div className='flex justify-center'>
                   <Link href='https://kurashi.city.nanto.toyama.jp/experience/' passHref>
                     <img
