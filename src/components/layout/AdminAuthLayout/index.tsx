6777ac6a5c2f18ce0d063a699fd30bc8116b6359
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-29 14:32:11 +0900

    🏷️  コンポーネント型定義

--- src/components/layout/AdminAuthLayout/index.tsx
+++ src/components/layout/AdminAuthLayout/index.tsx
@@ -1,7 +1,9 @@
+import { VFC } from 'react';
+
 type Props = {
   children: React.ReactNode;
 };
 
-export const AdminAuthLayout = (props: Props) => {
+export const AdminAuthLayout: VFC<Props> = (props) => {
   return <div className='flex h-screen'>{props.children}</div>;
 };
--- src/components/layout/UserLayout/index.tsx
+++ src/components/layout/UserLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Header } from 'src/components/layout/Header';
 import { Footer } from 'src/components/layout/Footer';
+import { VFC } from 'react';
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const UserLayout = (props: Props) => {
+export const UserLayout: VFC<Props> = (props) => {
   return (
     <div>
       <Header />
