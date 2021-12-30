40722cbdf0fb76dc1c4b29b84b1edc9516010c59
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-12-30 16:11:59 +0900

    🏷️  型定義変更

--- src/types/signin.tsx
+++ src/types/signin.tsx
@@ -1,7 +1,12 @@
 export type Signin = {
   email: string;
   password: string;
-  avatar_url: string;
+  // avatar_url: string;
   prefecture: string;
   group: string;
+  setPrefecture: (prefecture: string) => void;
+  setGroup: (group: string) => void;
+  setEmail: (email: string) => void;
+  setPassword: (password: string) => void;
+  handleProfileEdit: () => void;
 };
