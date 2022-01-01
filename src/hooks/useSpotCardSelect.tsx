1931db2db1c8f83e0a3aa1b406184fb9dfe2a99a
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-01-01 18:09:41 +0900

    ⚡️ データがない場合の条件追加

--- src/hooks/useSpotCardSelect.tsx
+++ src/hooks/useSpotCardSelect.tsx
@@ -2,6 +2,9 @@ import { supabase } from 'src/libs/supabase';
 
 export const getSpots = async () => {
   const { data, error } = await supabase.from('spots').select('*');
+  if (!data) {
+    return;
+  }
   if (error) {
     alert(error);
   }
