fc9a51318ef850e7673d3a72757e1c43fa484405
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-01-03 21:01:18 +0900

    ✨   コンポーネント追加

--- src/pages/spots/index.tsx
+++ src/pages/spots/index.tsx
@@ -2,10 +2,12 @@ import { NextPage } from 'next';
 import { SpotCard } from 'src/components/Spot/SpotCard';
 import { SpotPagination } from 'src/components/Spot/SpotPagination';
 import { UserLayout } from 'src/components/layout/UserLayout';
+import { SearchButton } from 'src/components/Button/SearchButton';
 
 const Spots: NextPage = () => {
   return (
     <UserLayout>
+      <SearchButton />
       <SpotCard />
       <SpotPagination />
     </UserLayout>
