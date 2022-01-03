1cd06432543f72b1209bc6a61bc6d0f1a629848f
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-01-03 20:47:31 +0900

    ✨  絞り込みボタン追加

---
+++ src/components/Button/SearchButton/index.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { VFC } from 'react';
+
+export const SearchButton: VFC = () => {
+  return (
+    <Flex
+      bg={useColorModeValue('#FFF', 'white')}
+      p={10}
+      w='full'
+      alignItems='right'
+      justifyContent='right'
+    >
+      <div className='flex'>
+        <button className='flex text-sm pl-2 pr-4 py-3 bg-white border-gray-400 border-2 rounded-3xl'>
+          <img src='/search.png' alt='スポット検索アイコン' className='mx-2 h-4 w-4 mt-0.5' />
+          絞り込む
+        </button>
+      </div>
+    </Flex>
+  );
+};
