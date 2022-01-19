9061b970063c43d451d99931674d59e086b1d12f
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-01-19 10:41:07 +0900

    🏷️ テーブル結合時のデータ型の型指定変更

--- src/hooks/useCategorySelect.tsx
+++ src/hooks/useCategorySelect.tsx
@@ -3,7 +3,7 @@ import { Category } from 'src/types/Category';
 
 export const getCategory = async () => {
   const { data, error } = await supabase
-    .from<Category>('spots')
+    .from<any>('spots')
     .select('prefecture_id (prefectures_name), system_id(systems_name)');
   if (!data) {
     return;
