f6dc79c89cdbc69abd46c9e65113ea0eb154cf16
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-01-21 11:42:25 +0900

    ⚗ propsのデータ受け取り

--- src/components/Category/PrefectureButton/index.tsx
+++ src/components/Category/PrefectureButton/index.tsx
@@ -6,11 +6,17 @@ type CategoryProps = {
 };
 
 export const PrefectureButton: VFC<CategoryProps> = (props) => {
-  console.log(props.category);
+  // console.log(props.category);
 
   return (
     <button className=' bg-red-400 text-white text-xs font-bold py-2 px-5 rounded '>
-      {props.category.prefecture_id.prefectures_name}
+      {props.category.prefecture_id}
+      {console.log(props.category.prefecture_id)}
+
+      {/* {props.category.prefecture_id['prefectures_name']} */}
+      {/* {props.category.prefecture_id.prefectures_name} */}
+      {/* {props.category.prefectures_name} */}
+      {/* {props.category} */}
     </button>
   );
 };
