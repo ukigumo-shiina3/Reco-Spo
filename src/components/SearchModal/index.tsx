73acb0b49b38db974b46a4960d16d8f3c35882c1
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-01-22 16:55:01 +0900

    ✨カテゴリーリスト名の表示

--- src/components/SearchModal/index.tsx
+++ src/components/SearchModal/index.tsx
@@ -1,27 +1,46 @@
 import { Dialog, Transition } from '@headlessui/react';
 import { Button, IconX } from '@supabase/ui';
-import { Fragment, useCallback, useState, VFC } from 'react';
-import { supabase } from 'src/libs/supabase';
+import { Fragment, useCallback, useEffect, useState, VFC } from 'react';
+import { getPrefectures } from 'src/hooks/usePrefectureSelect';
+import { getSystems } from 'src/hooks/useSystemSelect';
 import { SearchButton } from '../Button/SearchButton';
 
 const SearchModal: VFC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [prefecture, setPrefecture] = useState<string>('');
-  const [system, setSystem] = useState<string>('');
+  const [prefectures_name, setPrefecturesName] = useState([]);
+  const [systems_name, setSystemsName] = useState([]);
 
   const openModal = useCallback(() => {
     setIsOpen(true);
   }, []);
 
   const closeModal = useCallback(() => {
-    setPrefecture('');
-    setSystem('');
+    setPrefecturesName([]);
+    setSystemsName([]);
     setIsOpen(false);
   }, []);
 
+  const fetchPrefecturesListName = useCallback(async () => {
+    const data: string[] | null = await getPrefectures();
+    setPrefecturesName(data || []);
+  }, [setPrefecturesName]);
+
+  useEffect(() => {
+    fetchPrefecturesListName();
+  }, [fetchPrefecturesListName]);
+
+  const fetchSystemsListName = useCallback(async () => {
+    const data: string[] | null = await getSystems();
+    setSystemsName(data || []);
+  }, [setSystemsName]);
+
+  useEffect(() => {
+    fetchSystemsListName();
+  }, [fetchSystemsListName]);
+
   return (
     <>
-      <div className='p-2 border cursor-pointer' onClick={openModal}>
+      <div className='p-2 border cursor-pointer font-bold' onClick={openModal}>
         <SearchButton />
       </div>
 
@@ -40,42 +59,55 @@ const SearchModal: VFC = () => {
               leaveFrom='opacity-100 scale-100'
               leaveTo='opacity-0 scale-95'
             >
-              <div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform border border-gray-300 shadow-xl bg-gray-50 rounded-xl'>
-                <Dialog.Title
-                  as='h3'
-                  className='text-2xl font-medium leading-6 text-center text-gray-900'
-                >
-                  絞り込み検索
-                </Dialog.Title>
-                <div className='grid grid-cols-4 gap-2 mt-4'>
-                  <div className='col-span-1 text-xl text-center'>都道府県</div>
-                  <input
-                    className='w-full h-10 col-span-3 p-2 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700'
-                    value={prefecture}
-                    onChange={(e) => {
-                      return setPrefecture(e.target.value);
-                    }}
-                  />
+              <div className='inline-block w-full sm:w-96 max-w-md py-8 px-16 my-8 overflow-hidden text-left align-middle transition-all transform border border-gray-300 shadow-xl bg-gray-50 rounded-xl'>
+                <div className='flex'>
+                  <div className=''>
+                    <Dialog.Title
+                      as='h3'
+                      className='text-2xl font-medium leading-6 text-center text-gray-900 pb-10'
+                    >
+                      絞り込み検索
+                    </Dialog.Title>
+                  </div>
+                  <div className='w-16 pl-8'>
+                    <Button
+                      block
+                      type='default'
+                      size='large'
+                      icon={<IconX />}
+                      onClick={closeModal}
+                    ></Button>
+                  </div>
                 </div>
-                <div className='grid grid-cols-4 gap-2 mt-4'>
-                  <div className='col-span-1 text-xl text-center'>制度</div>
-                  <input
-                    className='w-full h-10 col-span-3 p-2 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700'
-                    value={system}
-                    onChange={(e) => {
-                      return setSystem(e.target.value);
-                    }}
-                  />
+
+                <div className='mt-4'>
+                  <button className='text-md text-center bg-red-400 rounded-l-md text-white py-4 px-8 border-2'>
+                    都道府県
+                  </button>
+                </div>
+                <div className='mt-4 flex flex-wrap gap-3'>
+                  {prefectures_name.map((value) => (
+                    <option key={value} value={value['id']}>
+                      {value['prefectures_name']}
+                    </option>
+                  ))}
+                </div>
+                <div className='mt-4'>
+                  <button className='text-md text-center bg-red-400 rounded-l-md text-white py-4 px-8'>
+                    制度
+                  </button>
+                </div>
+                <div className='mt-4 flex flex-wrap gap-3'>
+                  {systems_name.map((value) => (
+                    <option key={value} value={value['id']}>
+                      {value['systems_name']}
+                    </option>
+                  ))}
                 </div>
                 <div className='flex justify-center mt-4'>
                   <div className='w-32 p-2'>
-                    <Button block type='default' size='large' icon={<IconX />} onClick={closeModal}>
-                      閉じる
-                    </Button>
-                  </div>
-                  <div className='w-32 p-2'>
                     <Button block size='large'>
-                      検索
+                      絞り込む
                     </Button>
                   </div>
                 </div>
