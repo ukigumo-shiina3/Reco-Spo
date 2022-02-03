c03920eda0b3c65614cffedadae3e66d6869cdbb
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-02-03 11:58:16 +0900

    ♻️テーブル結合時のデータ取得変更

--- src/types/spot.tsx
+++ src/types/spot.tsx
@@ -1,20 +1,28 @@
 export type Spot = {
   id: string;
+  address: string;
   admin_id: string;
-  prefecture_id: string;
-  system_id: string;
-  name: string;
-  title: string;
-  image_url: string;
   appeal: string;
   area: string;
-  link: string;
-  target_person: string;
-  usage_fee: string;
-  term: string;
-  postal_code: string;
-  address: string;
-  manager: string;
-  tel: string;
+  created_at: string;
   email: string;
+  image_url: string;
+  link: string;
+  manager: string;
+  name: string;
+  postal_code: string;
+  prefecture_id: string;
+  prefectures: {
+    prefectures_name: string;
+  };
+  system_id: string;
+  systems: {
+    systems_name: string;
+  };
+  target_person: string;
+  tel: string;
+  term: string;
+  title: string;
+  updated_at: string;
+  usage_fee: string;
 };
