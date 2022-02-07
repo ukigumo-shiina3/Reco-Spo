c4fa637bc4089c173c35663c933a55a3d3f6d50c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-02-07 21:41:39 +0900

    🏷️ データ取得の型定義変更

--- src/hooks/useSystemSelect.tsx
+++ src/hooks/useSystemSelect.tsx
@@ -1,7 +1,8 @@
 import { supabase } from 'src/libs/supabase';
+import { Systems } from 'src/types/systems';
 
 export const getSystems = async () => {
-  const { data, error } = await supabase.from('systems').select('id, systems_name');
+  const { data, error } = await supabase.from<Systems>('systems').select('id, systems_name');
   if (error) {
     alert(error);
   }
