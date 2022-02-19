c0ee281134a18ce2d32e53be3163eec11d59540b
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-02-19 21:13:45 +0900

    🚧 ローディング時の処理追加

--- src/components/layout/UserLayout/index.tsx
+++ src/components/layout/UserLayout/index.tsx
@@ -1,17 +1,29 @@
 import { Header } from 'src/components/Layout/Header';
 import { Footer } from 'src/components/Layout/Footer';
-import { VFC } from 'react';
+import { useCallback, useEffect, useState, VFC } from 'react';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const UserLayout: VFC<Props> = (props) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const handleLoading = useCallback(() => {
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    handleLoading();
+  }, [handleLoading]);
+
   return (
     <div>
+      {/* {loading ? null : ( */}
       <Header />
       {props.children}
       <Footer />
+      {/* )}   */}
     </div>
   );
 };
