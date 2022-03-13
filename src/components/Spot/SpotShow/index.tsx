d31e7ce7b7d72050b43b15e3603ba0b317926cdc
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-03-13 21:14:20 +0900

    💄 レイアウトの調整

--- src/components/Spot/SpotShow/index.tsx
+++ src/components/Spot/SpotShow/index.tsx
@@ -10,7 +10,7 @@ type SpotCardProps = {
 export const SpotShow: VFC<SpotCardProps> = (props) => {
   return (
     <div>
-      <div className='w-screen sm:p-5 sm:px-10 py-5'>
+      <div className='w-screen sm:p-5 sm:px-10 sm:py-5'>
         <Image
           src='/spot-show-heading1.png'
           quality={100}
@@ -18,22 +18,7 @@ export const SpotShow: VFC<SpotCardProps> = (props) => {
           height={50}
           alt='アピールポイント'
         />
-        <p className='mb-2 text-sm md:text-lg '>{props.spot.appeal}</p>
-        {/* {console.log(props.spot)} */}
-
-        {/* <p className='mb-2 text-sm md:text-lg '>
-          世界農業遺産「能登の里山里海」に位置する石川県穴水町は美しい山と海に恵まれ、
-        </p>
-        <p className='mb-2 text-sm md:text-lg'>穏やかな時間が流れています。</p>
-        <p className='mb-2 text-sm md:text-lg'>
-          そんな穴水町の暮らしぶりを気軽に体験して頂けるように「短期移住体験住宅」をご用意いたしました。
-        </p>
-        <p className='mb-2 text-sm md:text-lg'>
-          空港や駅へのアクセスも良く、暮らしやすい場所で移住体験ができます。
-        </p>
-        <p className='mb-2 text-sm md:text-lg'>
-          田舎への移住をお考えの方はこの機会にぜひご利用下さい。
-        </p> */}
+        <p className='mb-2 text-sm md:text-xl '>{props.spot.appeal}</p>
       </div>
 
       <img
@@ -51,37 +36,34 @@ export const SpotShow: VFC<SpotCardProps> = (props) => {
             height={50}
             alt='スポット詳細'
           />
-          <p className='mb-2 text-sm md:text-lg'>住所：{props.spot.address}</p>
-          <p className='mb-2 text-sm md:text-lg'>物件：詳細はこちら{props.spot.link}</p>
-          <p className='mb-2 text-sm md:text-lg'>対象者：{props.spot.target_person}</p>
-          <p className='mb-2 text-sm md:text-lg'>利用料：{props.spot.usage_fee}</p>
-          <p className='mb-2 text-sm md:text-lg'>期間：{props.spot.term}</p>
-          {/* <p className='mb-2 text-sm md:text-lg'>住所：穴水町字岩車11-19-38[位置図]</p>
-          <p className='mb-2 text-sm md:text-lg'>物件：詳細はこちら</p>
-          <p className='mb-2 mt-8 text-sm md:text-lg'>◇対象者：町外から当町への移住を希望する者</p>
-          <p className='mb-2 text-sm md:text-lg'>◇利用料：無料</p>
-          <p className='mb-2 text-sm md:text-lg'>◇期間：最長７泊８日</p> */}
+          <div className='text-sm md:text-xl'>
+            <p className='mb-2 '>住所：{props.spot.address}</p>
+            <p className='mb-2'>物件：詳細はこちら{props.spot.link}</p>
+            <p className='mb-2'>対象者：{props.spot.target_person}</p>
+            <p className='mb-2'>利用料：{props.spot.usage_fee}</p>
+            <p className='mb-2'>期間：{props.spot.term}</p>
+          </div>
         </div>
-        <div className='p-5 sm:px-10 py-5'>
-          <Image
-            src='/spot-show-heading3.png'
-            quality={100}
-            width={180}
-            height={50}
-            alt='お問い合わせ先'
-          />
-          <p className='mb-2 text-sm md:text-lg'>お問合せはこちらまで</p>
-          <p className='mb-2 text-sm md:text-lg '>
-            住所： 〒{props.spot.postal_code} {props.spot.address}
-          </p>
-          <p className='mb-2 text-sm md:text-lg '>担当名: {props.spot.manager}</p>
-          <p className='mb-2 text-sm md:text-lg '>電話番号: {props.spot.tel}</p>
-          <p className='mb-2 text-sm md:text-lg '>メールアドレス: {props.spot.email}</p>
-          {/* 
-          <p className='mb-2 text-sm md:text-lg'>〒927-8601石川県鳳珠郡穴水町字川島ラの174番地</p>
-          <p className='mb-2 text-sm md:text-lg'>穴水町観光交流課</p>
-          <p className='mb-2 text-sm md:text-lg'>TEL:0768-52-3671 FAX:0768-52-2079</p>
-          <p className='mb-2 text-sm md:text-lg'>E-mail:kouryu3@sec.town.anamizu.ishikawa.jp</p> */}
+
+        <div className='sm:pl-4'>
+          <div className='p-5 sm:px-10 sm:py-5'>
+            <Image
+              src='/spot-show-heading3.png'
+              quality={100}
+              width={180}
+              height={50}
+              alt='お問い合わせ先'
+            />
+            <div className='text-sm md:text-xl'>
+              <p className='mb-2'>お問合せはこちらまで</p>
+              <p className='mb-2'>
+                住所： 〒{props.spot.postal_code} {props.spot.address}
+              </p>
+              <p className='mb-2'>担当名: {props.spot.manager}</p>
+              <p className='mb-2'>電話番号: {props.spot.tel}</p>
+              <p className='mb-2'>メールアドレス: {props.spot.email}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
