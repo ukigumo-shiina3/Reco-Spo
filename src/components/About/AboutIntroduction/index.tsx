79e00c3a161c0ea1c9d8687c07a7194fc4e75c49
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-04-15 10:04:35 +0900

    💄 文字の位置の修正

--- src/components/About/AboutIntroduction/index.tsx
+++ src/components/About/AboutIntroduction/index.tsx
@@ -8,9 +8,9 @@ export const AboutIntroduction: VFC = () => {
     <div>
       <figure className='relative w-full text-xs  sm:text-base '>
         <img src='/main-visual1.png' alt='サイト概要のメイン画像' className='w-full' />
-        <figcaption className='absolute text-white bottom-2 right-1 sm:bottom-4 sm:right-2 md:bottom-8  lg:bottom-12 lg:right-8 z-index-2  '>
+        <figcaption className='absolute text-white bottom-2 right-1 z-index-1 sm:bottom-4 sm:right-2 md:bottom-8 lg:bottom-12 lg:right-8 2xl:bottom-20'>
           <div className='hidden sm:block'>
-            <p className='font-black text-md sm:text-2xl md:text-3xl lg:text-4xl '>
+            <p className='font-black text-md sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl '>
               あなたの暮らしはここから始まる
             </p>
             <p className='font-bold pt-2 lg:pt-4 lg:text-xl xl:text-2xl '>
