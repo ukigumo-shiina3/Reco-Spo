9ac120e6005da01174d7983e866b8c6c6e6e914f
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-05-15 18:08:14 +0900

    🏷️ カラムに値がなくても入るように型定義変更

--- src/types/spotEdit.tsx
+++ src/types/spotEdit.tsx
@@ -1,24 +1,24 @@
 export type SpotEdit = {
   id: string;
-  prefecture_id: string;
+  prefecture_id?: string;
   prefectures: {
     prefectures_name: string[];
   };
-  system_id: string;
+  system_id?: string;
   systems: {
     systems_name: string[];
   };
-  name: string;
-  title: string;
-  appeal: string;
-  area: string;
-  link: string;
-  target_person: string;
-  usage_fee: string;
-  term: string;
-  postal_code: string;
-  address: string;
-  manager: string;
-  tel: string;
-  email: string;
+  name?: string;
+  title?: string;
+  appeal?: string;
+  area?: string;
+  link?: string;
+  target_person?: string;
+  usage_fee?: string;
+  term?: string;
+  postal_code?: string;
+  address?: string;
+  manager?: string;
+  tel?: string;
+  email?: string;
 };
