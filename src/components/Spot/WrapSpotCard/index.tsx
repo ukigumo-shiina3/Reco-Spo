332358dc5eb9171f596755fe3b752a862dc6f310
Yoshi <p.kitutune@gmail.com>
2022-05-21 15:05:29 +0900

    🎨 スポットを検索した後他のページに移動すると再び全スポットを取得し直す設定に変更

--- src/components/Spot/WrapSpotCard/index.tsx
+++ src/components/Spot/WrapSpotCard/index.tsx
@@ -3,12 +3,16 @@ import { SetStateAction, useCallback, useEffect, useState } from 'react';
 import { getSpots } from 'src/hooks/useSpotCardSelect';
 import { Spot } from 'src/types/spot';
 import { SimpleGrid } from '@mantine/core';
-import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable, useSetRecoilState } from 'recoil';
 import { searchValue } from 'src/recoil/atom';
 
 export const WrapSpotCard = () => {
   // Recoilからスポットで使う検索ワードを取得する
-  const searchWords = useRecoilValue(searchValue);
+  // 下の１と２をセットにしたものがuseRecoilState
+  //１ const searchWords = useRecoilValue(searchValue);
+  //２ const setSearchWords = useSetRecoilState(searchValue);
+  // atomの中身を取得してセットもする
+  const [searchWords, setSearchWords] = useRecoilState(searchValue);
   //  spotsデータを取得するstate
   const [spots, setSpots] = useState<Spot[]>([]);
 
@@ -29,7 +33,18 @@ export const WrapSpotCard = () => {
   useEffect(() => {
     fetchSpot();
   }, [fetchSpot]);
-
+  // アンマウント時にRecoilの中身を空にする：全検索に戻す
+  useEffect(() => {
+    return () => {
+      setSearchWords({
+        names: [],
+        system_ids: [],
+        is_data: false,
+      });
+    };
+    // 第二引数を空にして初回レンダリング時のみ動作させる
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     //  SpotCardコンポーネントの余白についてはこのコンポーネントを外から下記のように囲んでサイジングして上げるとわかりやすい
     // 　mtで上との余白　ml・mrで左右の余白
@@ -38,19 +53,23 @@ export const WrapSpotCard = () => {
     //   </div>
     //  tailwindcss基準　sm:640px md:768px lg:1024px xl:1280px
     <>
-      <SimpleGrid
-        cols={3} // カラム数
-        spacing='xl' // スペース xs sm md lg xl
-        breakpoints={[
-          { maxWidth: 1280, cols: 2, spacing: 'xl' },
-          { maxWidth: 768, cols: 1, spacing: 'xl' },
-        ]} // ブレークポイント
-      >
-        {spots.map((spot) => {
-          // console.log(spot);
-          return <SpotCard key={spot.id} spot={spot} />;
-        })}
-      </SimpleGrid>
+      {spots.length === 0 ? (
+        <>条件に一致するスポットが存在しません</>
+      ) : (
+        <SimpleGrid
+          cols={3} // カラム数
+          spacing='xl' // スペース xs sm md lg xl
+          breakpoints={[
+            { maxWidth: 1280, cols: 2, spacing: 'xl' },
+            { maxWidth: 768, cols: 1, spacing: 'xl' },
+          ]} // ブレークポイント
+        >
+          {spots.map((spot) => {
+            // console.log(spot);
+            return <SpotCard key={spot.id} spot={spot} />;
+          })}
+        </SimpleGrid>
+      )}
     </>
   );
 };
