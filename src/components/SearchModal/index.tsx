f97d28afeaa55817bd971be94a43ff4aef815003
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-05-23 21:53:37 +0900

    💄 レイアウト調整

--- src/components/SearchModal/index.tsx
+++ src/components/SearchModal/index.tsx
@@ -135,21 +135,21 @@ export const SearchModal: VFC = () => {
       <div className='flex justify-center sm:justify-end'>
         <div className='p-2 mt-8 cursor-pointer font-bold'>
           <div
-            className='ml-4 border-solid border-2 border-black inline-block px-4 py-2 md:px-5 md:py-3 mx-auto text-black hover:text-white bg-white  hover:bg-gray-400 rounded-full  md:mx-0'
+            className='ml-4 border-solid border-2 border-black inline-block px-4 py-2 md:px-5 md:py-3 mx-auto text-black hover:text-white bg-white  hover:bg-gray-400 rounded-full'
             onClick={resetSpots}
           >
             <span className='flex items-center '>
               <GrUpdate color={'#000'} />
-              　元に戻す
+              <p className='ml-4'>元に戻す</p>
             </span>
           </div>
           <div
-            className='ml-4 border-solid border-2 border-black inline-block px-4 py-2 md:px-5 md:py-3 mx-auto text-black hover:text-white bg-white  hover:bg-gray-400 rounded-full  md:mx-0'
+            className='ml-4 border-solid border-2 border-black inline-block px-4 py-2 md:px-5 md:py-3 mx-auto text-black hover:text-white bg-white  hover:bg-gray-400 rounded-full'
             onClick={openModal}
           >
             <span className='flex items-center '>
               <GoSearch color={'#000'} />
-              　絞り込む
+              <p className='ml-4'>絞り込む</p>
             </span>
           </div>
         </div>
