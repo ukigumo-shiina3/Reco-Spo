604bb0be1bbd67e8c4b3c937190aaef12c158ad7
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-06-07 22:11:37 +0900

    🔥 余分な記述の削除

--- src/pages/admins/index.tsx
+++ src/pages/admins/index.tsx
@@ -1,14 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
-import { ChangeEvent, useEffect, VFC } from 'react';
-// import { supabase } from 'src/libs/supabase';
-// import { toast } from 'react-hot-toast';
-// import Link from 'next/link';
-// import { Admin } from 'src/types/admin';
-// import { DEFAULT_AVATARS_BUCKET } from 'src/libs/constant';
-import { useCallback, useState } from 'react';
+import { useEffect, VFC } from 'react';
 import { Toaster } from 'react-hot-toast';
-import { Sidebar } from 'src/components/Layout/Sidebar';
 import UploadButton from 'src/components/Button/UploadButton/AdminUploadButton';
 import Avatar from 'src/components/Avatar';
 import { Spinner } from '@chakra-ui/react';
@@ -20,7 +13,6 @@ import { useAdminImage } from 'src/hooks/useAdminImage';
 import { AdminInfoLayout } from 'src/components/Layout/AdminInfoLayout';
 
 const schema = z.object({
-  // avatar_url: z.string().min(2, { message: 'アイコンURL は2文字以上の値で入力してください！' }),
   email: z.string().email({ message: '有効なメールアドレスを入力してください！' }),
   password: z.string().min(2, { message: 'パスワード は2文字以上の値で入力してください！' }),
   prefecture: z
@@ -31,7 +23,7 @@ const schema = z.object({
 });
 
 const ProfileEdit: VFC = () => {
-  const { admins, avatarUrl, loading, errorState } = useRecoil();
+  const { admins, loading } = useRecoil();
   const { uploading, uploadAvatar, avatarDownloadUrl, update } = useAdminImage();
   const form = useForm({
     schema: zodResolver(schema),
@@ -43,22 +35,6 @@ const ProfileEdit: VFC = () => {
       group: admins.group,
     },
   });
-  // const [admin, setAdmin] = useState<Admin>({
-  //   id: '',
-  //   avatar_url: '',
-  //   email: '',
-  //   password: '',
-  //   prefecture: '',
-  //   group: '',
-  // });
-
-  // const [loading, setLoading] = useState<boolean>(true);
-  // const [uploading, setUploading] = useState<boolean>(false);
-  // const [avatar, setAvatar] = useState<string | null>('');
-  // const [id, setId] = useState<string>('');
-  // const [error, setError] = useState(false);
-
-  // const user = supabase.auth.user();
 
   useEffect(() => {
     form.setValues({
@@ -71,110 +47,6 @@ const ProfileEdit: VFC = () => {
     });
   }, [admins]);
 
-  // useEffect(() => {
-  //   getProfile(id || '');
-  // }, [user]);
-
-  // const uploadAvatar = useCallback(async (event: ChangeEvent<HTMLInputElement>) => {
-  //   try {
-  //     setUploading(true);
-
-  //     if (!event.target.files || event.target.files.length == 0) {
-  //       throw '変更するプロフィール画像を選択してください';
-  //     }
-
-  //     const user = supabase.auth.user();
-  //     const file = event.target.files[0];
-  //     const fileExt = file.name.split('.').pop();
-  //     const fileName = `${Math.random()}.${fileExt}`;
-  //     const filePath = `${fileName}`;
-
-  //     const { error: uploadError } = await supabase.storage
-  //       .from(DEFAULT_AVATARS_BUCKET)
-  //       .upload(filePath, file);
-
-  //     if (uploadError) {
-  //       throw uploadError;
-  //     }
-
-  //     const { error: updateError } = await supabase
-  //       .from('admins')
-  //       .update({
-  //         avatar_url: filePath,
-  //       })
-  //       .eq('id', user?.id || '');
-
-  //     if (updateError) {
-  //       throw updateError;
-  //     }
-
-  //     setAvatar(filePath);
-  //   } catch (error) {
-  //     alert(error.message);
-  //   } finally {
-  //     setUploading(false);
-  //   }
-  // }, []);
-
-  // function setProfile(profile: Admin | null) {
-  //   if (!profile) {
-  //     return;
-  //   }
-  //   setAvatar(profile.avatar_url);
-  // }
-
-  // const getProfile = useCallback(async (id: string) => {
-  //   try {
-  //     setLoading(true);
-  //     const user = supabase.auth.user();
-
-  //     const { data, error } = await supabase
-  //       .from<Admin>('admins')
-  //       .select('id, avatar_url')
-  //       .eq('id', user?.id || '')
-  //       .single();
-
-  //     if (error) {
-  //       throw error;
-  //     }
-
-  //     setProfile(data);
-  //   } catch (error) {
-  //     console.log('error', error.message);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }, []);
-
-  // const handleProfileEdit = useCallback(async () => {
-  //   console.log(user?.id);
-
-  //   if (!user) return;
-
-  //   if (admin.prefecture === '' || admin.group === '' || admin.password === '') {
-  //     toast.error('入力されていない項目があります');
-  //   } else {
-  //     const { data, error } = await supabase
-  //       .from('admins')
-  //       .update({
-  //         avatar_url: admin.avatar_url,
-  //         prefecture: admin.prefecture,
-  //         group: admin.group,
-  //         email: admin.email,
-  //         password: admin.password,
-  //       })
-  //       .eq('id', user.id);
-  //     if (error) {
-  //       alert(error);
-  //     }
-  //     console.log({ data, error });
-
-  //     toast.success('プロフィールが更新されました', {
-  //       duration: 3000,
-  //     });
-  //   }
-  // }, [admin.prefecture, admin.group, admin.email, admin.password, user]);
-
   if (loading) {
     return (
       <div className='flex justify-center mt-64'>
@@ -182,16 +54,12 @@ const ProfileEdit: VFC = () => {
       </div>
     );
   }
-  // if (errorState) {
-  //   return <div>エラーが発生しました。</div>;
-  // }
 
   return (
     <>
       <AdminInfoLayout>
-        <h1 className='font-bold text-2xl mt-12'>プロフィール編集</h1>
+        <h1 className='font-bold text-3xl mt-12'>プロフィール編集</h1>
         <div className='h-full my-10 px-12 overflow-hidden shadow-lg bg-white'>
-          {/* <button onClick={() => setGroup(group + '1')}>test</button> */}
           <form
             onSubmit={form.onSubmit((values) => {
               console.log(values);
@@ -209,10 +77,6 @@ const ProfileEdit: VFC = () => {
               都道府県
             </label>
             <TextInput
-              // value={admin.prefecture}
-              // onChange={(e) => {
-              //   setAdmin({ ...admin, prefecture: e.target.value.trim() });
-              // }}
               required
               id='prefecture'
               placeholder='山形県'
@@ -223,10 +87,6 @@ const ProfileEdit: VFC = () => {
               自治体
             </label>
             <TextInput
-              // value={admin.group}
-              // onChange={(e) => {
-              //   setAdmin({ ...admin, group: e.target.value.trim() });
-              // }}
               required
               id='group'
               placeholder='遊佐町役場'
@@ -237,10 +97,6 @@ const ProfileEdit: VFC = () => {
               メールアドレス
             </label>
             <TextInput
-              // value={admin.email}
-              // onChange={(e) => {
-              //   setAdmin({ ...admin, email: e.target.value.trim() });
-              // }}
               required
               id='email'
               autoComplete='email'
@@ -252,10 +108,6 @@ const ProfileEdit: VFC = () => {
               パスワード
             </label>
             <TextInput
-              // value={admin.password}
-              // onChange={(e) => {
-              //   setAdmin({ ...admin, password: e.target.value.trim() });
-              // }}
               required
               id='password'
               type='password'
@@ -264,16 +116,7 @@ const ProfileEdit: VFC = () => {
               classNames={{ input: 'w-64 sm:w-96 p-2 rounded-l-md' }}
               {...form.getInputProps('password')}
             />
-            {/* <div className='text-center pb-10'>
-              <Link href='/admins' passHref>
-                <button
-                  onClick={handleProfileEdit}
-                  className=' px-4 py-2 mt-10 mx-6 text-white bg-blue-300 rounded-lg'
-                >
-                  変更
-                </button>
-              </Link>
-            </div> */}
+
             <div className='text-center pb-10'>
               <Button
                 type='submit'
