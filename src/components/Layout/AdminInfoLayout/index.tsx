509731caba6efe285c664a88ad019d67c52ae3c6
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-06-11 15:14:33 +0900

    💄モーダルのUI修正

--- src/components/Layout/AdminInfoLayout/index.tsx
+++ src/components/Layout/AdminInfoLayout/index.tsx
@@ -10,7 +10,6 @@ import { supabase } from 'src/libs/supabase';
 import { toast, Toaster } from 'react-hot-toast';
 import { useRouter } from 'next/router';
 import { Admin } from 'src/types/admin';
-import { useRecoil } from 'src/recoil/hooks';
 
 type Props = {
   children: React.ReactNode;
@@ -72,24 +71,18 @@ export const AdminInfoLayout: VFC<Props> = (props) => {
     () => (
       <>
         <div className='overflow-hidden shadow-lg bg-white'>
-          {/* <div className='w-full bg-white flex'> */}
           <Link href='/' passHref>
             <a className='block font-fancy font-bold text-4xl m-8'>Reco Spo</a>
           </Link>
-          {/* <button onClick={openModal}> */}
-          {/* <div className='' onClick={openModal}> */}
           <div className='' onClick={openModal}>
             <div className='absolute right-8 top-5 '>
               <Avatar url={avatarDownloadUrl} dummyImageUrl='/icons/profile-icon.png' size={70} />
             </div>
           </div>
-          {/* </button> */}
-          {console.log('openModal①', openModal)}
         </div>
-        {console.log('openModal②', openModal)}
         <Transition appear show={isOpen} as={Fragment}>
           <Dialog as='div' className='fixed inset-0 z-10 overflow-y-auto' onClose={closeModal}>
-            <div className='min-h-screen px-4 text-center border-2'>
+            <div className='min-h-screen px-4 border-2 rounded-3xl'>
               <span className='inline-block h-screen align-middle' aria-hidden='true'>
                 &#8203;
               </span>
@@ -102,17 +95,16 @@ export const AdminInfoLayout: VFC<Props> = (props) => {
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'
               >
-                <div className='absolute top-24 right-0 w-48 bg-white '>
+                <div className='absolute top-24 right-4 w-48 bg-gray-50 rounded-3xl'>
                   <div className='flex flex-col'>
-                    <div className='absolute right-4 '>
+                    <div className='absolute right-2 my-4'>
                       <button className='' onClick={closeModal}>
                         <div className='h-8 w-8'>✖︎</div>
                       </button>
-                      {console.log(closeModal)}
                     </div>
                     <Dialog.Title
                       as='h3'
-                      className='text-2xl pb-8 mt-12 border-b-2 border-gray-600'
+                      className='text-2xl pb-8 mt-12 border-b-2 border-gray-200 pl-4'
                     >
                       {adminData ? (
                         <p className='text-xs pb-2 md:text-sm'>{adminData?.group}様</p>
@@ -121,27 +113,30 @@ export const AdminInfoLayout: VFC<Props> = (props) => {
                       )}
                     </Dialog.Title>
                     <Link href='/admins/' passHref>
-                      <a className='text-xs hover:bg-gray-400-400 py-8 lg:text-sm '>
+                      <a className='text-xs hover:bg-gray-400-400 py-8  pl-4 lg:text-sm border-b-2  border-gray-200 '>
                         プロフィール編集
                       </a>
                     </Link>
                     <Link href='/admins/spots/post' passHref>
-                      <a className='text-xs hover:bg-gray-400 py-8 lg:text-sm '>スポット投稿</a>
+                      <a className='text-xs hover:bg-gray-400 py-8  pl-4 lg:text-sm border-b-2  border-gray-200 '>
+                        スポット投稿
+                      </a>
                     </Link>
                     <Link href={`/admins/spots/${adminId}/edit`} passHref>
-                      <a className='text-xs hover:bg-gray-400 py-8 lg:text-sm '>スポット編集</a>
+                      <a className='text-xs hover:bg-gray-400 py-8  pl-4 lg:text-sm border-b-2  border-gray-200 '>
+                        スポット編集
+                      </a>
                     </Link>
-                    <a className='text-xs hover:bg-gray-400 py-8 lg:text-sm '>
+                    <a className='text-xs hover:bg-gray-400 py-8  pl-4 lg:text-sm border-gray-200 '>
                       <button onClick={HandleLogout}>ログアウト</button>
                     </a>
                   </div>
+                  <Toaster />
                 </div>
-                {/* <Toaster /> */}
               </Transition.Child>
             </div>
           </Dialog>
         </Transition>
-        {console.log('isOpen', isOpen)}
 
         <div className='flex justify-center items-center h-screen bg-blue-50'>
           <div className='flex flex-col w-full justify-center items-center'>{props.children}</div>
