958eb76bfb6d2df53f8f7b093a68966a564eb861
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-06-17 21:43:32 +0900

    💄 余白と文字サイズの調整

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import type { VFC } from 'react';
 import { toast, Toaster } from 'react-hot-toast';
@@ -16,22 +15,22 @@ export const Header: VFC = () => {
       <div className='flex p-8'>
         <div className=''>
           <Link href='/' passHref>
-            <a className='font-fancy font-bold text-4xl pt-8 pb-10 md:text-5xl'>Reco Spo</a>
+            <a className='font-fancy font-bold text-4xl pt-4 sm:pt-8 pb-10 md:text-5xl'>Reco Spo</a>
           </Link>
         </div>
-        <div className='flex absolute right-8 top-10'>
+        <div className='flex absolute right-8 top-10 '>
           <Link href='/about' passHref>
-            <a className='px-2 pb-6 text-sm '>
+            <a className='px-1 sm:px-2 pb-6 text-sm md:text-base lg:text-lg '>
               <div className='pl-1 pt-0.5'>About</div>
             </a>
           </Link>
           <Link href='/admins/signin' passHref>
-            <a className='px-2 pb-6 text-sm'>
+            <a className='px-1 sm:px-2 pb-6 text-sm md:text-base lg:text-lg'>
               <div className='pl-1 pt-0.5'>自治体登録</div>
             </a>
           </Link>
           {/* <Link href='/map' passHref> */}
-          <a className='px-2 pb-10 text-sm'>
+          <a className='px-1 sm:px-2 pb-10 text-sm md:text-base lg:text-lg'>
             <button onClick={handleProgress} className='pl-1 pt-0.5'>
               マップ検索
             </button>
