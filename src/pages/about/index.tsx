565cdcc1777a7fc16e75011c341c28a3465a611b
ukigumo-shiina3 <daisukeyao@outlook.jp>
2022-06-19 10:49:35 +0900

    ✨ 制度概要のカルーセル作成

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,32 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image';
 import { NextPage } from 'next';
+import { AboutCarousel } from 'src/components/Carousel/AboutCarousel';
 import { UserLayout } from 'src/components/Layout/UserLayout';
 
 const About: NextPage = () => {
   return (
     <UserLayout>
       <div>
-        <figure className='relative flex justify-center items-center w-full text-xs sm:text-base'>
-          <img
-            src='/main-visuals/main-visual0.png'
-            alt='サイト概要のメイン画像'
-            className='w-full'
-          />
-          <figcaption className='absolute hidden sm:block bg-indigo-300 opacity-90 p-8 z-index-1'>
-            <div className=''>
-              <p className='text-white font-bold pt-2 lg:pt-4 lg:text-xl xl:text-2xl'>
-                Reco Spoは思い描く理想に合った田舎暮らしを実現したい人と
-              </p>
-              <p className='text-white font-bold pt-2 lg:pt-4 lg:text-xl xl:text-2xl'>
-                居住支援制度を公表し居住者を増やしたい自治体を結びつける
-              </p>
-              <p className='text-white font-bold pt-2 lg:pt-4 lg:text-xl xl:text-2xl'>
-                プラットフォームです。
-              </p>
-            </div>
-          </figcaption>
-        </figure>
+        <AboutCarousel />
       </div>
     </UserLayout>
   );
